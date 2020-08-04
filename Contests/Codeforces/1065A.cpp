520bd6454ac25cfe161f0fd0170f9a7994480757
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-08-04 16:20:12 -0400

    Create 1065A.cpp

---
+++ Contests/Codeforces/1065A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define ii pair<int,int>
+#define F first
+#define S second
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a) for(int i = a; i >= 0; i--)
+#define fori(i,m) for(auto i = m.begin(); i != m.end(); i++) 
+#define sz(s) int(s.size())
+#define cls(a,car) memset(a,car,sizeof (a))
+using namespace std;
+typedef long long ll;
+const int N = 1 * 1e6 + 5;
+const ll mod = 1e9 + 7;
+const int INF = INT_MAX;
+const ll INFCAD  = ll(INT_MAX) * 2 + 1;
+int main(){
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        ll s,a,b,c;
+        scanf("%lld %lld %lld %lld",&s,&a,&b,&c);
+        ll ans = s / c;
+        ll aux = ans / a;
+        ans += aux * b;
+        printf("%lld\n",ans);
+    }
+    return 0;
+}
