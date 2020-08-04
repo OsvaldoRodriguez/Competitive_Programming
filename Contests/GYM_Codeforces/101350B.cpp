df95f90562abf831f034ab670044dfcee4fa3dc4
OsvaldoRodriguez <rodriguezapazagroverosvaldo@gmail.com>
2020-08-04 08:03:46 -0400

    101450B

---
+++ Contests/GYM_Codeforces/101350B.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int N= 1000;
+int main(){
+  int a,b,t;
+  cin>>t;
+  while(t--){
+  cin>>a>>b;
+  if(a>=b)cout<<"FunkyMonkeys\n";
+  else cout<<"WeWillEatYou\n";
+}
+  return 0;
+}
