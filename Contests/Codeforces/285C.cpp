189005b9993a3b64bbc3c6de06df52a8d8c95e8a
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-08-04 19:33:53 -0400

    Create 285C.cpp

---
+++ Contests/Codeforces/285C.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define ii pair<long long,long long>
+#define F first
+#define S second
+#define pb push_back
+#define pf push_front
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a) for(int i = a; i >= 0; i--)
+#define fori(i,m) for(auto i = m.begin(); i != m.end(); i++) 
+#define sz(s) int(s.size())
+#define cls(a,car) memset(a,car,sizeof (a))
+using namespace std;
+typedef long long ll;
+const int N = 3 * 1e5 + 5;
+const ll mod = 1e9 + 7;
+const int INF = INT_MAX;
+const ll INFCAD  = ll(INT_MAX) * 2 + 1;
+int v[N];
+int main(){
+	int n;
+	scanf("%d",&n);
+	fore(i,0,n)
+		scanf("%d",v + i);
+	sort(v,v + n);
+	ll ans = 0;
+	for(ll i = 0; i < n; i++)
+		ans += abs(i + 1 - v[i]);
+	printf("%lld\n",ans);
+	return 0;
+}
+    
