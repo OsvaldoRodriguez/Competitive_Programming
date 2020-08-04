79a50cf27da5184e69115a9dc35eabf3e4282587
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-08-04 13:06:24 -0400

    Create 102397J.cpp

---
+++ Contests/GYM_Codeforces/102397J.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define ii pair<int,int>
+#define F first
+#define S second
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a) for(int i = a; i >= 0; i--)
+#define fori(i,m) for(auto i = m.begin(); i != m.end(); i++) 
+#define sz(s) int(s.size())
+#define cls(a,car) memset(a,car,sizeof (a))
+using namespace std;
+typedef long long ll;
+const int N = 1 * 1e5 + 5;
+const int mod = 1e9 + 7;
+const int INF = INT_MAX;
+const ll INFCAD  = ll(INT_MAX) * 2 + 1;
+int v[5];
+int main(){
+    fast;
+    ll n,x,a;
+    cin >> n >> x >> a;
+    ll entry = a / x;
+    ll ans = n / entry;
+    if(n % entry != 0)
+        ans++;
+    cout << ans << '\n';
+    return 0;
+}
