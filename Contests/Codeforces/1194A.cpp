558bb1585b8f4cd6bed06cd9af28e4cd80620e0e
OsvaldoRodriguez <rodriguezapazagroverosvaldo@gmail.com>
2020-08-03 22:53:19 -0400

    1194A

---
+++ Contests/Codeforces/1194A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int N =100;
+int main(){
+  int t;
+  ll n,x;
+  cin>>t;
+  while(t--){
+    cin>>n>>x;
+    cout<<x+x<<"\n";
+  }
+  return 0;
+}
