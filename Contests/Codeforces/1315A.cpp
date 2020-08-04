a91e4c09c34488a939a70aac07cf9181d0eecdd8
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-08-04 17:13:15 -0400

    Create 1315A.cpp

---
+++ Contests/Codeforces/1315A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define ii pair<int,int>
+#define F first
+#define S second
+#define pb push_back
+#define pf push_front
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a) for(int i = a; i >= 0; i--)
+#define fori(i,m) for(auto i = m.begin(); i != m.end(); i++) 
+#define sz(s) int(s.size())
+#define cls(a,car) memset(a,car,sizeof (a))
+using namespace std;
+typedef long long ll;
+const int N = 2 * 1e5 + 5;
+const ll mod = 1e9 + 7;
+const int INF = INT_MAX;
+const ll INFCAD  = ll(INT_MAX) * 2 + 1;
+int main(){
+    int t;
+    ll a,b,x,y;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%lld %lld %lld %lld",&a,&b,&x,&y);
+        printf("%lld\n",max(y * a,max((b - y - 1) * a,max(x * b,(a - x - 1) * b))));
+    }
+    return 0;
+}
+    
