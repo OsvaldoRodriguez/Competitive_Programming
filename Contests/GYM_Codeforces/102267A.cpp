4d61249ff5ca1a35a3dd41ee7558931971bb4c79
OsvaldoRodriguez <rodriguezapazagroverosvaldo@gmail.com>
2020-08-03 22:39:51 -0400

    102267A

---
+++ Contests/GYM_Codeforces/102267A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+typedef long long ll;
+const int N =30005;
+int main()
+{
+  int n,m;
+  while(cin>>n>>m){
+    printf(n>=m?"1\n":"0\n" );
+  }
+  return 0;
+}
