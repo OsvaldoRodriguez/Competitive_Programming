d95ae297ca80ed10ae9b288ea4808df347dc96d1
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-08-04 19:46:31 -0400

    Create 1326B.cpp

---
+++ Contests/Codeforces/1326B.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define ii pair<int,int>
+#define F first
+#define S second
+#define pb push_back
+#define pf push_front
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a) for(int i = a; i >= 0; i--)
+#define fori(i,m) for(auto i = m.begin(); i != m.end(); i++) 
+#define sz(s) int(s.size())
+#define cls(a,car) memset(a,car,sizeof (a))
+using namespace std;
+typedef long long ll;
+const int N = 1 * 1e5 + 5;
+const ll mod = 1e9 + 7;
+const int INF = INT_MAX;
+const ll INFCAD  = ll(INT_MAX) * 2 + 1;
+int main(){ 
+	int n;
+	scanf("%d",&n);
+	ll ans = 0;
+	fore(i,0,n){
+		ll x;
+		scanf("%lld",&x);
+		printf("%lld ",ans + x);
+		if(x > 0)
+			ans += x;
+	}
+	printf("\n");
+	return 0;
+}
+    
