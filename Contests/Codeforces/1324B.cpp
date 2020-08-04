3bbb8b219468d498ab5f0831783ce6ea8ae7cdba
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-08-04 19:15:29 -0400

    Create 1324B.cpp

---
+++ Contests/Codeforces/1324B.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define ii pair<int,int>
+#define F first
+#define S second
+#define pb push_back
+#define pf push_front
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a) for(int i = a; i >= 0; i--)
+#define fori(i,m) for(auto i = m.begin(); i != m.end(); i++) 
+#define sz(s) int(s.size())
+#define cls(a,car) memset(a,car,sizeof (a))
+using namespace std;
+typedef long long ll;
+const int N = 1 * 1e5 + 5;
+const ll mod = 1e9 + 7;
+const int INF = INT_MAX;
+const ll INFCAD  = ll(INT_MAX) * 2 + 1;
+int main(){
+	int t;
+	scanf("%d",&t);
+	while(t--){
+		int n;
+		scanf("%d",&n);
+		int v[n + 2];
+		fore(i,0,n)
+			scanf("%d",v + i);
+		bool flag = 0;
+		for(int i = 0; i < n; i++){
+			for(int j = n - 1; j > i + 1; j--)
+				if(v[i] == v[j]){
+					flag = 1;
+					break;
+				}
+		}
+		if(flag)
+			printf("YES\n");
+		else
+			printf("NO\n");
+	}
+	return 0;
+}
+    
