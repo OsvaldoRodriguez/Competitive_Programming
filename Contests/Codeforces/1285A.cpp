cb52f68cf7316223bf7375a44c1c4b5b28610201
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-08-04 12:52:29 -0400

    Create 1285A.cpp

---
+++ Contests/Codeforces/1285A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define ii pair<int,int>
+#define F first
+#define S second
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a) for(int i = a; i >= 0; i--)
+#define fori(i,m) for(auto i = m.begin(); i != m.end(); i++) 
+#define sz(s) int(s.size())
+#define cls(a,car) memset(a,car,sizeof (a))
+using namespace std;
+typedef long long ll;
+const int N = 50;
+const int mod = 1e9 + 7;
+const int INF = INT_MAX;
+const ll INFCAD  = ll(INT_MAX) * 2 + 1;
+int main(){
+    int n;
+    string s;
+    cin >> n >> s;
+    int r = 0,l = 0;
+    for(int i = 0; i < n; i++)
+        if(s[i] == 'R')
+            r++;
+        else l++;
+    cout << r + l + 1 << '\n';
+    return 0;
+}
