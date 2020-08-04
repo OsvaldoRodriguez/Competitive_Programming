218158ebf2ff7e797ca80323e26afc370ad0366e
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-08-04 16:48:16 -0400

    Create 750A.cpp

---
+++ Contests/Codeforces/750A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define ii pair<int,int>
+#define F first
+#define S second
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a) for(int i = a; i >= 0; i--)
+#define fori(i,m) for(auto i = m.begin(); i != m.end(); i++) 
+#define sz(s) int(s.size())
+#define cls(a,car) memset(a,car,sizeof (a))
+using namespace std;
+typedef long long ll;
+const int N = 1 * 1e5 + 5;
+const ll mod = 1e9 + 7;
+const int INF = INT_MAX;
+const ll INFCAD  = ll(INT_MAX) * 2 + 1;
+int main(){  
+    int n,min;
+    scanf("%d %d",&n,&min);
+    int tot = 240 - min;
+    int i = 1;
+    int ans = 0;
+    int tmp = 0;
+    while(tmp + i * 5 <= tot && n > 0){
+        tmp += i * 5;
+        i++;
+        n--;
+        ans++;
+    }
+    printf("%d\n",ans);
+    return 0;
+}
+    
