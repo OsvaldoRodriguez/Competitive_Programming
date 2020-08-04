0e13dc9eed02af600a9bdf59c9d2d40c97a3246c
OsvaldoRodriguez <rodriguezapazagroverosvaldo@gmail.com>
2020-08-03 20:34:33 -0400

    84A

---
+++ Contests/Codeforces/84A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+typedef long long ll;
+const int N = 100005;
+int main()
+{
+  int n;
+  while(cin>>n)
+    cout<<n/2*3<<endl;
+  return 0;
+}
