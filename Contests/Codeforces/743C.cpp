9bc2d461a02101551ecebb0b36380e318b06466b
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-08-04 22:12:49 -0400

    Create 743C.cpp

---
+++ Contests/Codeforces/743C.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define ii pair<int,int>
+#define F first
+#define S second
+#define pb push_back
+#define pf push_front
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a) for(int i = a; i >= 0; i--)
+#define fori(i,m) for(auto i = m.begin(); i != m.end(); i++) 
+#define sz(s) int(s.size())
+#define cls(a,car) memset(a,car,sizeof (a))
+using namespace std;
+typedef long long ll;
+const int N = 2 * 1e5 + 5;
+const ll mod = 1e9 + 7;
+const int INF = INT_MAX;
+const ll INFCAD  = ll(INT_MAX) * 2 + 1;
+//freopen("","r",stdin);
+//freopen("","w",stdout);
+int main(){
+    ll n;
+    scanf("%lld",&n);
+    if(n == 1){
+        printf("-1\n");
+        return 0;
+    }
+    printf("%lld %lld %lld\n",n, n + 1,n * (n + 1));
+	return 0;
+}
