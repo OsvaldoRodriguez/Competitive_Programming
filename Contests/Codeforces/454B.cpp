380886405f6366b031b02e6a7beadea00789c168
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-08-05 18:37:21 -0400

    Create 454B.cpp

---
+++ Contests/Codeforces/454B.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#define fin ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ii pair<int,int>
+#define F first
+#define S second
+#define pb push_back
+#define pf push_front
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a) for(int i = a; i >= 0; i--)
+#define fori(i,m) for(auto i = m.begin(); i != m.end(); i++) 
+#define w(t) while(t--)
+#define sz(s) int(s.size())
+#define cls(a,car) memset(a,car,sizeof (a))
+#define what_is(x) cerr << #x << " is " << x << endl;
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+const int N = 1 * 1e5 + 5;
+const ll mod = 1e9 + 7;
+const int INF = INT_MAX;
+const ll INFCAD  = ll(INT_MAX) * 2 + 1;
+const double E = 1e-9;
+// fflush(stdout)
+// cout << flush
+// a + b = a ^ b + 2 * (a & b)
+int main(){
+    //freopen("in","r",stdin);
+    //freopen("out","w",stdout);
+    fin;
+    int n;
+    cin >> n;
+    int v[2 * n + 10];
+    fore(i, 0, n){
+        cin >> v[i];
+    }
+    int i = 0;
+        int j = i + 1;
+        while(j < n && v[j] >= v[j - 1])
+            j++;
+        i = j;
+        int first = v[i];
+        int idx = i;
+        if(i == n){
+            cout << "0\n";
+            return 0;
+        }
+        j = i + 1;
+        while(j < n && v[j] >= v[j - 1]){
+            j++;
+        }
+        i = j;
+        first = v[i - 1];
+    if(i == n && first <= v[0]){
+        cout << n - idx << '\n';
+    }else
+        cout << -1 << '\n';
+    cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    return 0;
+}   
