6ee3f9aa14f0a1910c9e4f55e6f71bb7ed190fdf
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-08-05 16:23:48 -0400

    Create 1099A.cpp

---
+++ Contests/Codeforces/1099A.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define fin ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define ii pair<int,int>
+#define F first
+#define S second
+#define pb push_back
+#define pf push_front
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a) for(int i = a; i >= 0; i--)
+#define fori(i,m) for(auto i = m.begin(); i != m.end(); i++) 
+#define sz(s) int(s.size())
+#define cls(a,car) memset(a,car,sizeof (a))
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+const int N = 2 * 1e5 + 5;
+const ll mod = 1e9 + 7;
+const int INF = INT_MAX;
+const ll INFCAD  = ll(INT_MAX) * 2 + 1;
+const double E = 1e-9;
+int main(){
+    //freopen("in","r",stdin);
+    //freopen("out","w",stdout);
+    int w,h;
+    int u1, d1, u2, d2;
+    cin >> w >> h >> u1 >> d1 >> u2 >> d2;
+    while(h > 0){
+        w += h;
+        if(h == d1)
+            w -= u1;
+        if(h == d2)
+            w -= u2;
+        w = max(w, 0);
+        h--;
+    }
+    cout << w << '\n';
+    return 0;
+}   
