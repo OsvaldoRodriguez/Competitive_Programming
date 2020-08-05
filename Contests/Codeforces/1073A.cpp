ff90d0316e05e25368642011d12b5915e4f2b1b2
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-08-05 16:57:35 -0400

    Create 1073A.cpp

---
+++ Contests/Codeforces/1073A.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define fin ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ii pair<int,int>
+#define F first
+#define S second
+#define pb push_back
+#define pf push_front
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a) for(int i = a; i >= 0; i--)
+#define fori(i,m) for(auto i = m.begin(); i != m.end(); i++) 
+#define w(t) while(t--)
+#define sz(s) int(s.size())
+#define cls(a,car) memset(a,car,sizeof (a))
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+const int N = 2 * 1e5 + 5;
+const ll mod = 1e9 + 7;
+const int INF = INT_MAX;
+const ll INFCAD  = ll(INT_MAX) * 2 + 1;
+const double E = 1e-9;
+// fflush(stdout)
+// cout << flush
+// a + b = a ^ b + 2 * (a & b)
+int main(){
+    //freopen("in","r",stdin);
+    //freopen("out","w",stdout);
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    fore(i, 1, n){
+        if(s[i] != s[i - 1]){
+            cout << "YES\n";
+            cout << s[i - 1] << "" << s[i] << '\n';
+            return 0;
+        }
+    }
+    cout << "NO\n";
+    return 0;
+}   
