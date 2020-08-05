683c4d8953a22640a559ec6110ee963ddc1a9415
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-08-04 22:49:28 -0400

    Create 1144F.cpp

---
+++ Contests/Codeforces/1144F.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define ii pair<int,int>
+#define F first
+#define S second
+#define pb push_back
+#define pf push_front
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a) for(int i = a; i >= 0; i--)
+#define fori(i,m) for(auto i = m.begin(); i != m.end(); i++) 
+#define sz(s) int(s.size())
+#define cls(a,car) memset(a,car,sizeof (a))
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+const int N = 3 * 1e5 + 5;
+const ll mod = 1e9 + 7;
+const int INF = INT_MAX;
+const ll INFCAD  = ll(INT_MAX) * 2 + 1;
+const double E = 1e-9;
+vi G[N], vis(N, -1);
+bool flag = 1;
+void bfs(int n){
+    queue<int> q;
+    q.push(n);
+    vis[n] = 0;
+    while(sz(q)){
+        int u = q.front();q.pop();
+        for(int v : G[u])
+            if(vis[v] == -1){
+                q.push(v);
+                vis[v] = vis[u] ^ 1;
+            }else if(vis[v] == vis[u])
+                flag = 0;
+    }
+}
+int main(){
+    //freopen("in","r",stdin);
+    //freopen("out","w",stdout);
+    fast;
+    int n, m, u, v;
+    cin >> n >> m;
+    vi aux;
+    fore(i, 0, m){
+        cin >> u >> v;
+        u--;v--;
+        G[u].pb(v);
+        G[v].pb(u);
+        aux.pb(u);
+    }
+    bfs(0);
+    if(flag){
+        cout << "YES\n";
+        fore(i, 0, sz(aux)){
+            cout << (!vis[aux[i]]?'1':'0');
+        }
+    }else cout << "NO\n";
+    return 0;
+}   
