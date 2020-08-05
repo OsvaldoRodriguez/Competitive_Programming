0de610eb96d65367d1e6c69347d8796abccc22d1
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-08-05 19:28:36 -0400

    Create 222B.cpp

---
+++ Contests/Codeforces/222B.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#define fin ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ii pair<int,int>
+#define F first
+#define S second
+#define pb push_back
+#define pf push_front
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a) for(int i = a; i >= 0; i--)
+#define fori(i,m) for(auto i = m.begin(); i != m.end(); i++) 
+#define w(t) while(t--)
+#define sz(s) int(s.size())
+#define cls(a,car) memset(a,car,sizeof (a))
+#define what_is(x) cerr << #x << " is " << x << endl;
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+const int N = 1 * 1e5 + 5;
+const ll mod = 998244353;
+const int INF = INT_MAX;
+const ll INFCAD  = ll(INT_MAX) * 2 + 1;
+const double E = 1e-9;
+int dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
+// fflush(stdout)
+// cout << flush
+// a + b = a ^ b + 2 * (a & b)
+int occ[26], est[26];
+int main(){
+    //freopen("in","r",stdin);
+    //freopen("out","w",stdout);
+    fin;
+    int n, m, q;
+    cin >> n >> m >> q;
+    int v[n + 1][m + 1];
+    unordered_map<int, int> row, col;
+    fore(i, 1, n + 1)
+        row[i] = i;
+    fore(j, 1, m + 1)
+        col[j] = j;
+    fore(i, 1, n + 1)fore(j, 1, 1 + m)cin >> v[i][j];
+    w(q){
+        char s;
+        int a, b;
+        cin >> s >> a >> b;
+        if(s == 'g')
+            cout << v[row[a]][col[b]] << '\n';
+        else if(s == 'r'){
+            int aux = row[a];
+            row[a] = row[b];
+            row[b] = aux;
+        }else if(s == 'c'){
+            int aux = col[a];
+            col[a] = col[b];
+            col[b] = aux;
+        }
+    }
+    cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    return 0;
+}   
