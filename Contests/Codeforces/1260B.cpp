0f0b1e0c5b237f2bab659ed38d075636fd1c32d0
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-08-05 16:33:11 -0400

    Create 1260B.cpp

---
+++ Contests/Codeforces/1260B.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define fin ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ii pair<int,int>
+#define F first
+#define S second
+#define pb push_back
+#define pf push_front
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a) for(int i = a; i >= 0; i--)
+#define fori(i,m) for(auto i = m.begin(); i != m.end(); i++) 
+#define w(t) while(t--)
+#define sz(s) int(s.size())
+#define cls(a,car) memset(a,car,sizeof (a))
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+const int N = 1 * 1e5 + 5;
+const ll mod = 1e9 + 7;
+const int INF = INT_MAX;
+const ll INFCAD  = ll(INT_MAX) * 2 + 1;
+const double E = 1e-9;
+int main(){
+    //freopen("in","r",stdin);
+    //freopen("out","w",stdout);
+    fin;
+    int t;
+    cin >> t;
+    int a, b;
+    w(t){
+        cin >> a >> b;
+        if(a > b)
+            swap(a, b);
+        cout << ((a + b) % 3 == 0 && a * 2 >= b? "YES" : "NO" ) << '\n';
+    }
+    return 0;
+}   
