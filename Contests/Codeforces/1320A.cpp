3a5232f1659bf1b5f1031d469e9f1ab1d35406d3
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-08-05 21:41:13 -0400

    Create 1320A.cpp

---
+++ Contests/Codeforces/1320A.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define fin ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ii pair<int,int>
+#define F first
+#define S second
+#define pb push_back
+#define pf push_front
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a) for(int i = a; i >= 0; i--)
+#define fori(i,m) for(auto i = m.begin(); i != m.end(); i++) 
+#define w(t) while(t--)
+#define sz(s) int(s.size())
+#define cls(a,car) memset(a,car,sizeof (a))
+#define db(x) cerr << #x << " is " << x << '\n'
+#define FL freopen("in", "r", stdin),freopen("out", "w", stdout);
+#define EjecuteTime cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n"
+#define angle(x) double(x * acos(-1) / 180.0)
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+const int N = 2 * 1e5 + 5;
+const ll mod = 11092019;
+const int INF = INT_MAX;
+const ll INFCAD  = ll(INT_MAX) * 2 + 1;
+const double E = 1e-9;
+//int dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
+//int dx[] = {2, 1, -1, -2, -2, -1, 1, 2}, dy[] = {1, 2, 2, 1, -1, -2, -2, -1};
+// fflush(stdout)
+// cout << flush
+// a + b = a ^ b + 2 * (a & b)
+int v[N];
+int main(){
+    //FL;
+    fin;
+    int n;
+    cin >> n;
+    map<ll, ll> m;
+    fore(i, 0, n){
+        ll x;
+        cin >> x;
+        m[x - i] += x;
+    }
+    ll ans = 0;
+    fori(i, m)
+        ans = max(ans, i -> S);
+    
+    cout << ans << '\n';
+    EjecuteTime;
+    return 0;
+}       
+
