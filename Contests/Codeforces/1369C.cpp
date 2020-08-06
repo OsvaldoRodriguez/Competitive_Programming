90e2f7beb4a08dfe61dbd79bb91d1d09309bf7c7
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-08-05 22:11:27 -0400

    Create 1369C.cpp

---
+++ Contests/Codeforces/1369C.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+#define FIN         ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ii          pair<int,int>
+#define F           first
+#define S           second
+#define pb          push_back
+#define all(x)      (x).begin(),(x).end()
+#define rall(x)     (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a)   for(int i = a; i >= 0; i--)
+#define fori(i,m)   for(auto i = m.begin(); i != m.end(); i++) 
+#define w(t)        while(t--)
+#define sz(s)       int(s.size())
+#define cls(a,car)  memset(a,car,sizeof (a))
+#define db(x)       cerr << #x << " is " << x << '\n'
+#define FL          freopen("input.txt", "r", stdin),freopen("output.txt", "w", stdout);
+#define EjecuteTime cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n"
+#define angle(x)    double(x * acos(-1) / 180.0)
+using namespace std;
+typedef long long   ll;
+typedef vector<int> vi;
+typedef vector<ii>  vii;
+const int N = 2 * 1e5 + 5;
+const ll mod = 1e9 + 7;
+const double E = 1e-9;
+int dx[] = {1, 0}, dy[] = {0, 1};
+//int dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
+//int dx[] = {2, 1, -1, -2, -2, -1, 1, 2}, dy[] = {1, 2, 2, 1, -1, -2, -2, -1};
+// fflush(stdout)
+// cout << flush
+// a + b = a ^ b + 2 * (a & b)
+int add(int a, int b){return  a + b > mod ? a + b - mod : a + b;}
+int mul(int a, int b){return a * b >= mod ? a * b % mod : a * b;}  
+int v[N];
+int went[N];
+int main(){
+    //FL;
+    FIN;
+    int n, t, k;
+    cin >> t;
+    w(t){
+        cin >> n >> k;
+        ll ans = 0;
+        for(int i = 0; i < n; i++)cin >> v[i];
+        for(int i = 0; i < k; i++)cin >> went[i];
+        sort(v, v + n);
+        for(int i = 0; i < k; i++){
+            if(went[i] == 1){
+                ans += 2LL * v[n - 1];
+                n--;
+            }
+        }
+        sort(went, went + k);
+        reverse(went, went + k);
+        int i = 0, j = n - 1;
+        int mx = -1;
+        int mn = 1e9;
+        for(int x = 0; x < k; x++){
+            if(went[x] == 1)continue;
+            mx = v[j--];
+            went[x]--;
+            if(went[x] != 0)
+                mn = 1e9;
+            else
+                mn = mx;
+            /*db(i);
+            db(j);*/
+            while(went[x] > 0){
+                mn = min(mn, v[i++]);
+                went[x]--;
+            }
+            ans += 0ll + mx + mn;
+        //    db(ans);
+        }
+        cout << ans << '\n';
+
+    }
+    EjecuteTime;
+    return 0;
+}       
