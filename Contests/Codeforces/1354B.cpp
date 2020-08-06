43570e9975074769e971cbd41abd5931943e2440
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-08-05 20:51:41 -0400

    Create 1354B.cpp

---
+++ Contests/Codeforces/1354B.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#define fin ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ii pair<int,int>
+#define F first
+#define S second
+#define pb push_back
+#define pf push_front
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a) for(int i = a; i >= 0; i--)
+#define fori(i,m) for(auto i = m.begin(); i != m.end(); i++) 
+#define w(t) while(t--)
+#define sz(s) int(s.size())
+#define cls(a,car) memset(a,car,sizeof (a))
+#define ff(x) cerr << #x << " is " << x << '\n'
+#define FL freopen("in", "r", stdin),freopen("out", "w", stdout);
+#define time cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n"
+using namespace std;
+typedef unsigned long long ll;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+const int N = 2 * 1e5 + 5;
+const ll mod = 1e9 + 7;
+const int INF = INT_MAX;
+const ll INFCAD  = ll(INT_MAX) * 2 + 1;
+const double E = 1e-9;
+int dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
+// fflush(stdout)
+// cout << flush
+// a + b = a ^ b + 2 * (a & b) 
+int main(){
+    //FL;
+    fin;
+    int t;
+    cin >> t;
+    w(t){
+        string s;
+        cin >> s;
+        int v[5], idx[5];
+        cls(v, 0);
+        cls(idx, 0);
+        int ans = 1e9;
+        fore(i, 0, sz(s)){
+            if(v[1] && v[2] && v[3]){
+                ans = min(ans, max(idx[3], max(idx[1], idx[2])) - min(idx[3], min(idx[1], idx[2])) + 1);
+            }
+            idx[s[i] - '0'] = i;
+            v[s[i] - '0'] = 1;
+        }
+        if(v[1] && v[2] && v[3]){
+            ans = min(ans, max(idx[3], max(idx[1], idx[2])) - min(idx[3], min(idx[1], idx[2])) + 1); 
+        }
+        if(ans == 1e9)
+            ans = 0;
+        cout << ans << '\n';
+    }
+    time;
+    return 0;
+}   
