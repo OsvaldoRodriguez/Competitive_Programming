6c2bc82482580e495108d09ad67b357a7e18a848
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-08-05 20:30:19 -0400

    Create 1260C.cpp

---
+++ Contests/Codeforces/1260C.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define fin ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ii pair<int,int>
+#define F first
+#define S second
+#define pb push_back
+#define pf push_front
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a) for(int i = a; i >= 0; i--)
+#define fori(i,m) for(auto i = m.begin(); i != m.end(); i++) 
+#define w(t) while(t--)
+#define sz(s) int(s.size())
+#define cls(a,car) memset(a,car,sizeof (a))
+#define what_is(x) cerr << #x << " is " << x << endl;
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+const int N = 1 * 1e6 + 5;
+const ll mod = 1e9 + 7;
+const int INF = INT_MAX;
+const ll INFCAD  = ll(INT_MAX) * 2 + 1;
+const double E = 1e-9;
+int dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
+// fflush(stdout)
+// cout << flush
+// a + b = a ^ b + 2 * (a & b)
+
+int main(){
+    //freopen("in","r",stdin);
+    //freopen("out","w",stdout);
+    fin;
+    int t;
+    cin >> t;
+    w(t){
+        int r, b, k;
+        cin >> r >> b >> k;
+        if(r > b)
+            swap(r, b);
+        int gcd = __gcd(r, b);
+        r /= gcd;
+        b /= gcd;
+        cout << ((b - 1) / r + ((b - 1) % r != 0) >= k? "REBEL" : "OBEY") << '\n';
+    }
+    cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    return 0;
+}   
