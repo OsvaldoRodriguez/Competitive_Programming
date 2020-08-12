4fe7fe9e4c4869aed633a242cd7fc5768efaad08
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-08-12 01:05:37 -0400

    Update COINS.cpp

--- Contests/Spoj/COINS.cpp
+++ Contests/Spoj/COINS.cpp
@@ -1,23 +1,49 @@
 #include <bits/stdc++.h>
+#define FIN         ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ii          pair<int,int>
+#define F           first
+#define S           second
+#define pb          push_back
+#define all(x)      (x).begin(),(x).end()
+#define rall(x)     (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a)   for(int i = a; i >= 0; i--)
+#define fori(i,m)   for(auto i = m.begin(); i != m.end(); i++) 
+#define w(t)        while(t--)
+#define sz(s)       int(s.size())
+#define cls(a,car)  memset(a,car,sizeof (a))
+#define db(x)       cerr << #x << " is " << x << '\n'
+#define FL          freopen("input.txt", "r", stdin),freopen("output.txt", "w", stdout);
+#define EjecuteTime cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n"
+#define angle(x)    double(x * acos(-1) / 180.0)
 using namespace std;
-typedef long long ll;
-const int N = 10000001;
-map<int,ll > m;
-ll maxi(ll a,ll b){
-  if(a>b)
-    return a;
-  return b;
-}
+typedef long long   ll;
+typedef vector<int> vi;
+typedef vector<ii>  vii;
+const int N = 1 * 1e4 + 5;
+const ll mod = 1e9 + 7;
+const double E = 1e-9;
+//int dx[] = {1, 0}, dy[] = {0, 1};
+//int dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
+//int dx[] = {2, 1, -1, -2, -2, -1, 1, 2}, dy[] = {1, 2, 2, 1, -1, -2, -2, -1};
+// fflush(stdout)
+// cout << flush
+// a + b = a ^ b + 2 * (a & b)
+map<int, ll> dp;
 ll f(int n){
-  if(n==0)return 0;
-  if(m[n]!=0)return m[n];
-  return m[n]=maxi(n,f(n/2)+f(n/3)+f(n/4));
+    if(n <= 11)
+        return n;
+    if(dp.count(n))
+        return dp[n];
+    return dp[n] = f(n / 2) + f(n / 3) + f(n / 4);
+ 
 }
-int main()
-{
-  int n;
-  while(cin>>n){
-    cout<<f(n)<<endl;
-  }
-  return 0;
-}
+int main(){
+    //FL;
+    FIN;
+    int n;
+    while(cin >> n)
+        cout << f(n) << '\n';
+    EjecuteTime;
+    return 0;
+} 
