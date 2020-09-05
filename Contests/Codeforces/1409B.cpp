53b08262606b14c1865dceca5511c50a9e88a427
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-09-04 23:30:24 -0400

    Create 1409B.cpp

---
+++ Contests/Codeforces/1409B.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#define FIN         ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ii          pair<int,int>
+#define F           first
+#define S           second
+#define pb          push_back
+#define all(x)      (x).begin(),(x).end()
+#define rall(x)     (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a)   for(int i = a; i >= 0; i--)
+#define fori(i,m)   for(auto i = m.begin(); i != m.end(); i++) 
+#define w(t)        while(t--)
+#define sz(s)       int(s.size())
+#define cls(a,car)  memset(a,car,sizeof (a))
+#define db(x)       cerr << #x << " is " << x << '\n'
+#define FI          freopen("input.txt", "r", stdin)
+#define FO          freopen("output.txt", "w", stdout)
+#define EjecuteTime cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n"
+#define angle(x)    double(x * acos(-1) / 180.0)
+using namespace std;
+typedef long long   ll;
+typedef vector<int> vi;
+typedef vector<ii>  vii;
+const int N = 1e4 + 5;
+const ll mod = 1e9 + 7;
+const double E = 1e-9;
+//int dx[] = {0, -1}, dy[] = {-1, 0};
+//int dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
+//int dx[] = {1, 1, 0, -1, -1, -1, 0, 1}, dy[] = {0, 1, 1, 1, 0, -1, -1, -1};
+//int dx[] = {2, 1, -1, -2, -2, -1, 1, 2}, dy[] = {1, 2, 2, 1, -1, -2, -2, -1};
+// fflush(stdout)
+// cout << flush
+// a + b = a ^ b + 2 * (a & b)
+
+ll go(ll a, ll x, ll b, ll y, ll n){
+    ll cur = min(a - x, n);
+    a -= cur;
+    n -= cur;
+    cur = min(b - y, n);
+    b -= cur;
+    return a * b;
+
+}
+void solve(){
+    ll a, b, x, y, n;
+    cin >> a >> b >> x >> y >> n;
+    ll ans = go(a, x, b, y, n);
+    ans = min(ans, go(b, y, a, x, n));
+    cout << ans << '\n';
+}   
+int main(){
+    FIN;
+    int t = 1;
+    cin >> t;
+    for(int i = 1; i <= t; i++){
+        solve();
+    }
+    EjecuteTime;
+    return 0;
+}       
