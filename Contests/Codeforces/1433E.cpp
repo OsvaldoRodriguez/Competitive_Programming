f03806626ce13552f6d76bd8b05ae13164d3481e
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-10-21 22:36:25 -0400

    Create 1433E.cpp

---
+++ Contests/Codeforces/1433E.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#define FIN         ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ii          pair<int,int>
+#define F           first
+#define S           second
+#define pb          push_back
+#define all(x)      (x).begin(),(x).end()
+#define rall(x)     (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a)   for(int i = a; i >= 0; i--)
+#define fori(i,m)   for(auto i = m.begin(); i != m.end(); i++) 
+#define w(t)        while(t--)
+#define sz(s)       int(s.size())
+#define cls(a,car)  memset(a,car,sizeof (a))
+#define db(x)       cerr << #x << " is " << x << '\n'
+#define FI          freopen("input.txt", "r", stdin)
+#define FO          freopen("output.txt", "w", stdout)
+#define EjecuteTime cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n"
+#define angle(x)    double(x * acos(-1) / 180.0)
+using namespace std;
+typedef long long   ll;
+typedef vector<int> vi;
+typedef vector<ii>  vii;
+const int N = 5e3 + 5;
+const ll mod = 1e9 + 7;
+const double E = 1e-7;
+const int INF = 1e2;
+//int dx[] = {0, -1}, dy[] = {-1, 0};
+//int dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
+//int dx[] = {1, 1, 0, -1, -1, -1, 0, 1}, dy[] = {0, 1, 1, 1, 0, -1, -1, -1};
+//int dx[] = {2, 1, -1, -2, -2, -1, 1, 2}, dy[] = {1, 2, 2, 1, -1, -2, -2, -1};
+// fflush(stdout)
+// cout << flush
+// a + b = a ^ b + 2 * (a & b)
+//105470163083
+//100000000223257 primos
+//1000000000318157
+ll f(ll n){
+    ll ans = 1;
+    fore(i, 1, n + 1)
+        ans *= i;
+    return ans;
+}
+bool isPowerOfTwo(int x){
+    if(x&(x-1)) 
+        return 0;
+    return 1;
+}
+ll _f(ll x){
+    ll ans = 0;
+    while(x > 0){
+        x /= 2;
+        ans++;
+    }
+    return ans;
+}
+void solve(){
+    ll n;
+    cin >> n;
+    cout << f(n - 1) / (n / 2) << '\n';
+
+}
+int main(){
+    FIN;
+    int t = 1;
+    //cin >> t;
+    for(int i = 1; i <= t; i++){
+        solve();
+    }
+    EjecuteTime;
+    return 0;
+
+}
