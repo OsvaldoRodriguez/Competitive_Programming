3c1da22318a41df585a098b9af81ec4a955521ca
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-10-23 22:25:13 -0400

    Create 988.cpp

---
+++ Contests/Uva/988.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+#define FIN         ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ii          pair<int,int>
+#define F           first
+#define S           second
+#define pb          push_back
+#define all(x)      (x).begin(),(x).end()
+#define rall(x)     (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a)   for(int i = a; i >= 0; i--)
+#define fori(i,m)   for(auto i = m.begin(); i != m.end(); i++) 
+#define w(t)        while(t--)
+#define sz(s)       int(s.size())
+#define cls(a,car)  memset(a,car,sizeof (a))
+#define db(x)       cerr << #x << " is " << x << '\n'
+#define FI          freopen("input.txt", "r", stdin)
+#define FO          freopen("output.txt", "w", stdout)
+#define EjecuteTime cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n"
+#define angle(x)    double(x * acos(-1) / 180.0)
+using namespace std;
+typedef long long   ll;
+typedef vector<int> vi;
+typedef vector<ii>  vii;
+const int N = 2e5 + 5;
+const ll mod = 1e9 + 7;
+const double E = 1e-7;
+const int INF = 1e2;
+//int dx[] = {0, -1}, dy[] = {-1, 0};
+//int dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
+//int dx[] = {1, 1, 0, -1, -1, -1, 0, 1}, dy[] = {0, 1, 1, 1, 0, -1, -1, -1};
+//int dx[] = {2, 1, -1, -2, -2, -1, 1, 2}, dy[] = {1, 2, 2, 1, -1, -2, -2, -1};
+// fflush(stdout)
+// cout << flush
+// a + b = a ^ b + 2 * (a & b)
+//105470163083
+//100000000223257 primos
+//1000000000318157
+vi G[N];
+int out[N];
+ll dp[N];
+int n;
+ll f(int u){
+    if(out[u] == 0)
+        return 1;
+    ll &ans = dp[u];
+    if(ans == -1){
+        ans = 0;
+        for(int v : G[u])
+            ans += f(v);
+    }
+    return ans;
+}
+void solve(){
+    int len, x;
+    int test = 1;
+    while(cin >> n){
+        for(int i = 0; i < n; i++){
+            G[i].clear();
+            out[i] = 0;
+        }
+        for(int i = 0; i < n; i++){
+            cin >> len;
+            for(int j = 0; j < len; j++){
+                cin >> x;
+                G[i].pb(x);
+                out[i]++;
+            }
+        }
+        cls(dp, -1);
+        ll ans = f(0);
+        if(test > 1)
+            cout << '\n';
+        test++;
+        cout << ans << '\n';
+        
+    }
+}
+int main(){
+    FIN;
+    int t = 1;
+    //cin >> t;
+    for(int i = 1; i <= t; i++){
+        solve();
+    }
+    EjecuteTime;
+    return 0;
+
+}
