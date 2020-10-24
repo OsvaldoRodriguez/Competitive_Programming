8e65c54a6e813789091509312471cc432055f4fc
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-10-23 23:03:05 -0400

    Update ELIS n ^2 to n log n

--- Contests/Spoj/ELIS.cpp
+++ Contests/Spoj/ELIS.cpp
@@ -1,29 +1,55 @@
 #include <bits/stdc++.h>
+#define FIN         ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ii          pair<int,int>
+#define F           first
+#define S           second
+#define pb          push_back
+#define all(x)      (x).begin(),(x).end()
+#define rall(x)     (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a)   for(int i = a; i >= 0; i--)
+#define fori(i,m)   for(auto i = m.begin(); i != m.end(); i++) 
+#define w(t)        while(t--)
+#define sz(s)       int(s.size())
+#define cls(a,car)  memset(a,car,sizeof (a))
+#define db(x)       cerr << #x << " is " << x << '\n'
+#define FI          freopen("input.txt", "r", stdin)
+#define FO          freopen("output.txt", "w", stdout)
+#define EjecuteTime cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n"
+#define angle(x)    double(x * acos(-1) / 180.0)
 using namespace std;
-typedef long long ll;
-const int N=11;
-int dp[N],v[N];
-int f(int i){
-  if(dp[i]!=-1)
-    return dp[i];
-  int sol=1;
-  for(int j=0;j<i;j++){
-    if(v[j]<v[i]){
-      sol=max(sol,1+f(j));
+typedef long long   ll;
+typedef vector<int> vi;
+typedef vector<ii>  vii;
+const int N = 1e4 + 5;
+const ll mod = 1e9 + 7;
+const double E = 1e-7;
+const int INF = 1e2;
+int n;
+int v[N];
+void solve(){
+    cin >> n;
+    for(int i = 0; i < n; i++)cin >> v[i];
+    vi L(1, 1e9);
+    for(int i = 0; i < n; i++){
+        int pos = lower_bound(all(L), v[i]) - L.begin();
+        if(pos == sz(L))
+            L.pb(v[i]);
+        else
+            L[pos] = v[i];
+ 
     }
-  }
-  return dp[i]=sol;
+    cout << sz(L) << '\n';
+ 
 }
-int main()
-{
-  int n;
-  cin>>n;
-  memset(dp,-1,sizeof dp);
-  for(int i=0;i<n;i++)
-    cin>>v[i];
-  int ans=0;
-  for(int i=0;i<n;i++)
-    ans=max(ans,f(i));
-  cout<<ans<<endl;
-  return 0;
-}
+int main(){
+    FIN;
+    int t = 1;
+    //cin >> t;
+    for(int i = 1; i <= t; i++){
+        solve();
+    }
+    //EjecuteTime;
+    return 0;
+ 
+} 
