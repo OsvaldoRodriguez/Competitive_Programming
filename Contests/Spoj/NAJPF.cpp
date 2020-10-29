31b510b2a34fe4ba8febe85dff1b0555619e8be3
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-10-29 14:50:07 -0400

    Update NAJPF.cpp

--- Contests/Spoj/NAJPF.cpp
+++ Contests/Spoj/NAJPF.cpp
@@ -1,80 +1,74 @@
 #include <bits/stdc++.h>
+#define FIN         ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ii          pair<int,int>
+#define F           first
+#define S           second
+#define pb          push_back
+#define all(x)      (x).begin(),(x).end()
+#define rall(x)     (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a)   for(int i = a; i >= 0; i--)
+#define fori(i,m)   for(auto i = m.begin(); i != m.end(); i++) 
+#define w(t)        while(t--)
+#define sz(s)       int(s.size())
+#define cls(a,car)  memset(a,car,sizeof (a))
+#define db(x)       cerr << #x << " is " << x << '\n'
+#define FI          freopen("input.txt", "r", stdin)
+#define FO          freopen("output.txt", "w", stdout)
+#define EjecuteTime cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n"
+#define angle(x)    double(x * acos(-1) / 180.0)
 using namespace std;
-const int N=1000010;
-int lps[N];
-string patron,texto;
-vector<int> v;
-bool flag=0;
-void LPS(){
-  int M=patron.size();
-  lps[0]=0;
-  int i=1,len=0;
-  while(i<M){
-    if(patron[i]==patron[len]){
-      len++;
-      lps[i]=len;
-      i++;
+typedef long long   ll;
+typedef vector<int> vi;
+typedef vector<ii>  vii;
+const int N = 2e2 + 5;
+const ll mod = 1e9 + 7;
+const double E = 1e-7;
+const int INF = 1e2;
+vector<int> get_phi(string &p){
+    vector<int> phi(sz(p));
+    for(int i = 1, j = 0; i < sz(p); i++){
+        while(j > 0 && p[i] != p[j])j = phi[j - 1];
+        if(p[i] == p[j])j++;
+        phi[i] = j;
     }
-    else
-    {
-      if(len!=0)
-        len=lps[len-1];
-      else
-      {
-        lps[i]=0;
-        i++;
-      }
-    }
-  }
- 
+    return phi;
 }
-void KMP(){
-  int M=patron.size();
-  int N=texto.size();
-  LPS();
-  int i=0,j=0;
-  while(i<N){
-    if(texto[i]==patron[j]){
-      i++;j++;
-    }
-    if(j==M){
-      v.push_back(i-j+1);
-      flag=1;
-      j=lps[j-1];
-    }
-    else
-    {
-      if(i<N && texto[i]!=patron[j]){
-        if(j!=0)
-          j=lps[j-1];
-        else
-          i++;
-      }
-    }
  
-  }
+vector<int> get_match(string &t, string &p){
+    vector<int> mathes, phi = get_phi(p);
+    for(int i = 0, j = 0; i < sz(t); i++){
+        while(j > 0 && t[i] != p[j])j = phi[j - 1];
+        if(t[i] == p[j])j++;
+        if(j == sz(p)){
+            mathes.pb(i - sz(p) + 1);
+            j = phi[j - 1];
+        }
+    }
+    return mathes;
 }
-int main()
-{
-  int test,t=1;
-  cin>>test;
-  while(test--){
-    flag=0;
-    v.clear();
-    cin>>texto>>patron;
-    KMP();
-    if(t>1)
-      cout<<"\n";
-    t++;
-    if(flag){
-    cout<<v.size()<<"\n";
-    for(int i=0;i<v.size();i++)
-      cout<<v[i]<<" ";
-    cout<<"\n";
-  }
-  else
-    cout<<"Not Found\n";
+void solve(){   
+    string t, p;
+    cin >> t >> p;
+    vector<int> ans = get_match(t, p);
+    if(!sz(ans)){
+        cout << "Not Found\n";
+        return;
+    }
+    cout << sz(ans) << '\n';
+    for(auto i : ans)cout << i + 1 << ' ';
+    cout << '\n';
+    
+}
+int main(){
+    FIN;
+    int t = 1;
+    cin >> t;
+    for(int i = 1; i <= t; i++){
+        if(i > 1)cout <<  '\n';
+        solve();
+    }
+    //EjecuteTime;
+    return 0;
  
-  }
-  return 0;
-}
+} 
