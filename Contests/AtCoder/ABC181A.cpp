2da655490bde1cbfcd5404447051846121802ed1
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2020-11-07 15:57:49 -0400

    Create ABC181A.cpp

---
+++ Contests/AtCoder/ABC181A.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define ii              pair<int,int>
+#define F               first
+#define S               second
+#define pb              push_back
+#define all(x)          (x).begin(),(x).end()
+#define rall(x)         (x).rbegin(),(x).rend()
+#define fore(i, a, b)   for(int i = a; i < b; i += 1)
+#define forr(i, a)      for(int i = a; i >= 0; i--)
+#define sz(s)           int(s.size())
+#define cls(a,car)      memset(a,car,sizeof (a))
+#define db(x)           cerr << #x << " is " << x << '\n'
+#define angle(x)        double(x * acos(-1) / 180.0)
+using namespace std;
+typedef long long   ll;
+typedef vector<int> vi;
+typedef vector<ii>  vii;
+const int N = 2e5 + 5;
+const ll mod = 1e9 + 7;
+const double E = 1e-7;
+const int oo = 1e9;
+void solve(){
+    int n;
+    cin >> n;
+    cout << (n & 1 ? "Black" : "White") << '\n';
+}
+int main(){
+    #ifdef LOCAL
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #else
+        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #endif
+ 
+    int t = 1;
+    //cin >> t;
+    for(int i = 1; i <= t; i++)
+        solve();
+ 
+    #ifdef LOCAL
+        cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+    return 0;
+}
