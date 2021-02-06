294507f3b319641d625e5610fccb64c33f152780
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2021-02-06 16:09:29 -0400

    Create Tower_Breakers.cpp

---
+++ Contests/HackerRank/Tower_Breakers.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define ii pair<int,int>
+#define F first
+#define S second
+#define pb push_back
+#define pf push_front
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a) for(int i = a; i >= 0; i--)
+#define fori(i,m) for(auto i = m.begin(); i != m.end(); i++) 
+#define sz(s) int(s.size())
+#define cls(a,car) memset(a,car,sizeof (a))
+using namespace std;
+typedef long long ll;
+const int N = 2 * 1e5 + 5;
+const ll mod = 1e9 + 7;
+const int INF = INT_MAX;
+const ll INFCAD  = ll(INT_MAX) * 2 + 1;
+//freopen("","r",stdin);
+//freopen("","w",stdout);
+int main(){
+    int t;
+    scanf("%d",&t);
+    int n,m;
+    while(t--){
+        scanf("%d %d",&n,&m);
+        if(m == 1)
+            printf("2\n");
+        else
+        printf("%d\n",n & 1?1:2);
+    }
+	return 0;
+}
