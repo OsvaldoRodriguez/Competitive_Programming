0ac639b16666ea11fc29037d5ed54cc32827771c
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2021-10-02 14:59:23 -0400

    Update 514.cpp

--- Contests/Uva/514.cpp
+++ Contests/Uva/514.cpp
@@ -1,36 +1,26 @@
 #include <bits/stdc++.h>
-#define FIN         ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define ii          pair<int,int>
-#define F           first
-#define S           second
-#define pb          push_back
-#define all(x)      (x).begin(),(x).end()
-#define rall(x)     (x).rbegin(),(x).rend()
-#define fore(i,a,b) for(int i = a;i < b; i+= 1)
-#define forr(i,a)   for(int i = a; i >= 0; i--)
-#define fori(i,m)   for(auto i = m.begin(); i != m.end(); i++) 
-#define w(t)        while(t--)
-#define sz(s)       int(s.size())
-#define cls(a,car)  memset(a,car,sizeof (a))
-#define db(x)       cerr << #x << " is " << x << '\n'
-#define FI          freopen("input.txt", "r", stdin)
-#define FO          freopen("output.txt", "w", stdout)
-#define EjecuteTime cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n"
-#define angle(x)    double(x * acos(-1) / 180.0)
+#define ii              pair<int,int>
+#define F               first
+#define S               second
+#define pb              push_back
+#define all(x)          (x).begin(),(x).end()
+#define rall(x)         (x).rbegin(),(x).rend()
+#define sz(s)           int(s.size())
+#define cls(a, car)     memset(a,car,sizeof (a))
+#define db(x)           cout << #x << " is " << x << '\n'
+#define angle(x)        double(x * acos(-1) / 180.0)
 using namespace std;
-typedef unsigned long long   ll;
+void debug(){cout << endl;}
+template<typename T, typename... Args>
+void debug(T a, Args... args){cout << a << " "; debug(args...);}
+typedef long long   ll;
 typedef vector<int> vi;
 typedef vector<ii>  vii;
-const int N = 1e3 + 5;
-const ll mod = 1e9 + 7;
-const double E = 1e-9;
-//int dx[] = {0, -1}, dy[] = {-1, 0};
-//int dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
-//int dx[] = {1, 1, 0, -1, -1, -1, 0, 1}, dy[] = {0, 1, 1, 1, 0, -1, -1, -1};
-//int dx[] = {2, 1, -1, -2, -2, -1, 1, 2}, dy[] = {1, 2, 2, 1, -1, -2, -2, -1};
-// fflush(stdout)
-// cout << flush
-// a + b = a ^ b + 2 * (a & b)
+const int N = 2e5 + 15;
+const ll mod = INT_MAX;
+const double E = 1e-7;
+
+
 int v[N];
 void solve(){
     int n;
@@ -73,13 +63,20 @@ void solve(){
     }
 }
 int main(){
-    FIN;
+    #ifdef FURY
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #else
+        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #endif
     int t = 1;
-    //cin >> t;
-    while(t--){
+    // cin >> t;
+    for(int i = 1; i <= t; i++){ 
         solve();
-        cout << '\n';
     }
-    EjecuteTime;
+    cout << '\n';
+    #ifdef FURY
+        cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
     return 0;
-}       
+}
