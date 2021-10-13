32663aeafdd96bbab6123e4edcf3451fd92349e1
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2021-10-13 01:08:34 -0400

    Create A. Chat room.cpp

---
+++ Contests/Codeforces/Codeforces Beta Round #54 (Div. 2)/A. Chat room.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define FIN         ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ii          pair<int,int>
+#define F           first
+#define S           second
+#define pb          push_back
+#define all(x)      (x).begin(),(x).end()
+#define rall(x)     (x).rbegin(),(x).rend()
+#define fore(i,a,b) for(int i = a;i < b; i+= 1)
+#define forr(i,a)   for(int i = a; i >= 0; i--)
+#define fori(i,m)   for(auto i = m.begin(); i != m.end(); i++) 
+#define w(t)        while(t--)
+#define sz(s)       int(s.size())
+#define cls(a,car)  memset(a,car,sizeof (a))
+#define db(x)       cerr << #x << " is " << x << '\n'
+#define FL          freopen("input.txt", "r", stdin),freopen("output.txt", "w", stdout);
+#define EjecuteTime cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n"
+#define angle(x)    double(x * acos(-1) / 180.0)
+using namespace std;
+typedef long long   ll;
+typedef vector<int> vi;
+typedef vector<ii>  vii;
+const int N = 1e5 + 50;
+const ll mod = 1e9 + 7;
+const double E = 1e-9;
+//int dx[] = {0, -1}, dy[] = {-1, 0};
+//int _dx[] = {0, 1}, _dy[] = {1, 0};
+//int dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
+//int dx[] = {1, 1, 0, -1, -1, -1, 0, 1}, dy[] = {0, 1, 1, 1, 0, -1, -1, -1};
+//int dx[] = {2, 1, -1, -2, -2, -1, 1, 2}, dy[] = {1, 2, 2, 1, -1, -2, -2, -1};
+// fflush(stdout)
+// cout << flush
+// a + b = a ^ b + 2 * (a & b)
+int main(){
+    //FL;
+    FIN;
+    string str, aux = "hello";
+    cin >> str;
+    int j = 0;
+    for(int i = 0; i < sz(str); i++){
+        if(str[i] == aux[j])
+            j++;
+    }
+    cout << (j == sz(aux) ? "YES" : "NO") << '\n';
+    EjecuteTime;
+    return 0;
+}       
