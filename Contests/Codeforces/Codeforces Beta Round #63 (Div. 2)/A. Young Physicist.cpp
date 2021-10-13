1dd02964237dd290a71aad1de80eb04eb9f7c0dd
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2021-10-13 01:15:52 -0400

    Create A. Young Physicist.cpp

---
+++ Contests/Codeforces/Codeforces Beta Round #63 (Div. 2)/A. Young Physicist.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    int n,x;
+    cin>>n;
+    int v[n];
+    memset(v,0,n*4);
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            cin>>x;
+            v[j]+=x;
+        }
+ 
+    }
+    bool sw=true;
+    for(int i=0;i<n;i++)
+    {
+        if(v[i]!=0)
+        {
+            sw=false;
+            cout<<"NO\n";
+            break;
+        }
+    }
+    if(sw)
+        cout<<"YES\n";
+    return 0;
+}
