7780d493faa8ac7b956ff0f08ad77ad496b12b30
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2021-11-26 20:52:56 -0400

    Update Disjoint_set_union.cpp

--- Notebook/data_structures/Disjoint_set_union.cpp
+++ Notebook/data_structures/Disjoint_set_union.cpp
@@ -1,28 +1,8 @@
 #include <bits/stdc++.h>
-#define FIN         ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define ii          pair<int,int>
-#define F           first
-#define S           second
-#define pb          push_back
-#define all(x)      (x).begin(),(x).end()
-#define rall(x)     (x).rbegin(),(x).rend()
-#define fore(i,a,b) for(int i = a;i < b; i+= 1)
-#define forr(i,a)   for(int i = a; i >= 0; i--)
-#define fori(i,m)   for(auto i = m.begin(); i != m.end(); i++) 
-#define w(t)        while(t--)
-#define sz(s)       int(s.size())
-#define cls(a,car)  memset(a,car,sizeof (a))
-#define db(x)       cerr << #x << " is " << x << '\n'
-#define FL          freopen("input.txt", "r", stdin),freopen("output.txt", "w", stdout);
-#define EjecuteTime cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n"
-#define angle(x)    double(x * acos(-1) / 180.0)
 using namespace std;
 typedef long long   ll;
 typedef vector<int> vi;
-typedef vector<ii>  vii;
 const int N = 1e5 + 50;
-const ll mod = 1e9 + 7;
-const double E = 1e-9;
 class UnionFind{
     private:
         vi p,rank,size;
@@ -30,7 +10,7 @@ class UnionFind{
     public:
         UnionFind(int N){
             //index 0
-            size.assign(N,1);//tamaño del conjunto
+            size.assign(N, 1);//tamaño del conjunto
             numSets = N;
             maxSubSet = 1;
             rank.assign(N,0);
