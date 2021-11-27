5bacda386b4eb8f3f7a80c57719acd408e887f36
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2021-11-26 21:04:43 -0400

    Update Binary_indexed_tree_2D(BIT).cpp

--- Notebook/data_structures/Binary_indexed_tree_2D(BIT).cpp
+++ Notebook/data_structures/Binary_indexed_tree_2D(BIT).cpp
@@ -1,28 +1,7 @@
 #include <bits/stdc++.h>
-#define FIN         ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define ii          pair<int,int>
-#define F           first
-#define S           second
-#define pb          push_back
-#define all(x)      (x).begin(),(x).end()
-#define rall(x)     (x).rbegin(),(x).rend()
-#define fore(i,a,b) for(int i = a;i < b; i+= 1)
-#define forr(i,a)   for(int i = a; i >= 0; i--)
-#define fori(i,m)   for(auto i = m.begin(); i != m.end(); i++) 
-#define w(t)        while(t--)
-#define sz(s)       int(s.size())
-#define cls(a,car)  memset(a,car,sizeof (a))
-#define db(x)       cerr << #x << " is " << x << '\n'
-#define FL          freopen("input.txt", "r", stdin),freopen("output.txt", "w", stdout);
-#define EjecuteTime cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n"
-#define angle(x)    double(x * acos(-1) / 180.0)
 using namespace std;
 typedef long long   ll;
-typedef vector<int> vi;
-typedef vector<ii>  vii;
 const int N = 1e3 + 50;
-const ll mod = 1e9 + 7;
-const double E = 1e-9;
 ll A[N][N], BIT[N][N];
 int n, m; 
 /*
@@ -55,8 +34,6 @@ int sum(int pos_i, int pos_j){
 }
 
 int main(){
-    //FL;
-    FIN;
     cin >> n >> m;
     for(int i = 1; i <= n; i++)
         for(int j = 1; j <= m; j++)
@@ -73,9 +50,7 @@ int main(){
         else{
             update(a, b, c - A[a][b]);
             A[a][b] = c;
-            }
         }
     }
-    EjecuteTime;
     return 0;
 }       
