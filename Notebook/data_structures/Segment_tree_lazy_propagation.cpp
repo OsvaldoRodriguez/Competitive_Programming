dfe77683a8153d1f10c7b5de9da29992b3fd9376
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2021-11-26 20:59:14 -0400

    Update Segment_tree_lazy_propagation.cpp

--- Notebook/data_structures/Segment_tree_lazy_propagation.cpp
+++ Notebook/data_structures/Segment_tree_lazy_propagation.cpp
@@ -1,28 +1,10 @@
 #include <bits/stdc++.h>
-#define FIN         ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 #define ii          pair<int,int>
-#define F           first
-#define S           second
-#define pb          push_back
-#define all(x)      (x).begin(),(x).end()
-#define rall(x)     (x).rbegin(),(x).rend()
-#define fore(i,a,b) for(int i = a;i < b; i+= 1)
-#define forr(i,a)   for(int i = a; i >= 0; i--)
-#define fori(i,m)   for(auto i = m.begin(); i != m.end(); i++) 
-#define w(t)        while(t--)
-#define sz(s)       int(s.size())
-#define cls(a,car)  memset(a,car,sizeof (a))
-#define db(x)       cerr << #x << " is " << x << '\n'
-#define FL          freopen("input.txt", "r", stdin),freopen("output.txt", "w", stdout);
-#define EjecuteTime cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n"
-#define angle(x)    double(x * acos(-1) / 180.0)
 using namespace std;
 typedef long long   ll;
 typedef vector<int> vi;
 typedef vector<ii>  vii;
 const int N = 1 * 1e4 + 5;
-const ll mod = 1e9 + 7;
-const double E = 1e-9;
 
 /*
 Complexity
@@ -107,8 +89,6 @@ struct SegTreeLazy{
 
 };
 int main(){
-    //FL;
-    FIN;
     int t;
     scanf("%d", &t);
     for(int i = 1; i <= t; i++){
