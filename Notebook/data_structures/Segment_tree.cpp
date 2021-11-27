346c67ac9449eb6a4ba3fcc158b8d6c037504d14
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2021-11-26 20:55:11 -0400

    Update Segment_tree.cpp

--- Notebook/data_structures/Segment_tree.cpp
+++ Notebook/data_structures/Segment_tree.cpp
@@ -1,28 +1,11 @@
 #include <bits/stdc++.h>
-#define FIN         ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 #define ii          pair<int,int>
-#define F           first
-#define S           second
-#define pb          push_back
-#define all(x)      (x).begin(),(x).end()
-#define rall(x)     (x).rbegin(),(x).rend()
-#define fore(i,a,b) for(int i = a;i < b; i+= 1)
-#define forr(i,a)   for(int i = a; i >= 0; i--)
-#define fori(i,m)   for(auto i = m.begin(); i != m.end(); i++) 
-#define w(t)        while(t--)
 #define sz(s)       int(s.size())
-#define cls(a,car)  memset(a,car,sizeof (a))
-#define db(x)       cerr << #x << " is " << x << '\n'
-#define FL          freopen("input.txt", "r", stdin),freopen("output.txt", "w", stdout);
-#define EjecuteTime cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n"
-#define angle(x)    double(x * acos(-1) / 180.0)
 using namespace std;
 typedef long long   ll;
 typedef vector<int> vi;
 typedef vector<ii>  vii;
 const int N = 1 * 1e4 + 5;
-const ll mod = 1e9 + 7;
-const double E = 1e-9;
 
 /*
 Complexity
@@ -93,8 +76,6 @@ struct SegTree{
 
 };
 int main(){
-    //FL;
-    FIN;
     int n, m;
     cin >> n >> m;
     vi v(n);
@@ -102,7 +83,7 @@ int main(){
         cin >> i;
     SegTree tree(n, v);
     tree.build();
-    w(m){
+    while(m--){
         int op, l, r;
         cin >> op >> l >> r;
         if(op == 1)
@@ -110,6 +91,5 @@ int main(){
         else
             cout << tree.get(l, r - 1) << '\n';
     }
-    EjecuteTime;
     return 0;
 }       
