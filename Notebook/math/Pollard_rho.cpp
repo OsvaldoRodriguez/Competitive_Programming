36ae4727e44b9b505f2097c1dd696c086d53fbdd
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2021-11-26 22:15:14 -0400

    Update Pollard_rho.cpp

--- Notebook/math/Pollard_rho.cpp
+++ Notebook/math/Pollard_rho.cpp
@@ -1,37 +1,7 @@
 #include <bits/stdc++.h>
-#define FIN         ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define ii          pair<int,int>
-#define F           first
-#define S           second
-#define pb          push_back
-#define all(x)      (x).begin(),(x).end()
-#define rall(x)     (x).rbegin(),(x).rend()
-#define fore(i,a,b) for(int i = a;i < b; i+= 1)
-#define forr(i,a)   for(int i = a; i >= 0; i--)
-#define fori(i,m)   for(auto i = m.begin(); i != m.end(); i++)
-#define forir(i,m)   for(auto i = m.rend(); i != m.rbegin(); i++) 
-#define w(t)        while(t--)
-#define sz(s)       int(s.size())
-#define cls(a,car)  memset(a,car,sizeof (a))
-#define db(x)       cerr << #x << " is " << x << '\n'
-#define FI          freopen("input.txt", "r", stdin)
-#define FO          freopen("output.txt", "w", stdout)
-#define EjecuteTime cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n"
-#define angle(x)    double(x * acos(-1) / 180.0)
 using namespace std;
 typedef long long   ll;
-typedef vector<int> vi;
-typedef vector<ii>  vii;
 const int N = 3e5 + 50;
-const ll mod = 1e9 + 7;
-const double E = 1e-6;
-//int dx[] = {0, -1}, dy[] = {-1, 0};
-//int dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
-//int dx[] = {1, 1, 0, -1, -1, -1, 0, 1}, dy[] = {0, 1, 1, 1, 0, -1, -1, -1};
-//int dx[] = {2, 1, -1, -2, -2, -1, 1, 2}, dy[] = {1, 2, 2, 1, -1, -2, -2, -1};
-// fflush(stdout)
-// cout << flush
-// a + b = a ^ b + 2 * (a & b)
 ll gcd(ll a, ll b){
     return !b ? a : gcd(b, a % b);
 }
@@ -107,22 +77,16 @@ void solve(){
     fact(x, f);
     bool flag = 0;
     for(auto i : f){
-        for(int j = 0; j < i.S; j++){    
+        for(int j = 0; j < i.second; j++){    
             if(flag)
                 cout << " ";
-            cout << i.F;
+            cout << i.first;
             flag = 1;
         }
     }
     cout << '\n';
 }
 int main(){
-    FIN;
-    int t = 1;
-    cin >> t;
-    for(int i = 1; i <= t; i++){
-        solve();
-    }
-    EjecuteTime;
+    solve();
     return 0;
-}       
+}  
