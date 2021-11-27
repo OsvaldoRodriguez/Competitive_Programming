3459ddb9cdacc59dabc1ec570fd269205fcb8500
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2021-11-26 21:43:34 -0400

    Update Detect_cycle_undirected_graph.cpp

--- Notebook/graphs/Detect_cycle_undirected_graph.cpp
+++ Notebook/graphs/Detect_cycle_undirected_graph.cpp
@@ -1,28 +1,8 @@
 #include <bits/stdc++.h>
-#define FIN         ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define ii          pair<int,int>
-#define F           first
-#define S           second
-#define pb          push_back
-#define all(x)      (x).begin(),(x).end()
-#define rall(x)     (x).rbegin(),(x).rend()
-#define fore(i,a,b) for(int i = a;i < b; i+= 1)
-#define forr(i,a)   for(int i = a; i >= 0; i--)
-#define fori(i,m)   for(auto i = m.begin(); i != m.end(); i++) 
-#define w(t)        while(t--)
-#define sz(s)       int(s.size())
-#define cls(a,car)  memset(a,car,sizeof (a))
-#define db(x)       cerr << #x << " is " << x << '\n'
-#define FL          freopen("input.txt", "r", stdin),freopen("output.txt", "w", stdout);
-#define EjecuteTime cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n"
-#define angle(x)    double(x * acos(-1) / 180.0)
 using namespace std;
 typedef long long   ll;
 typedef vector<int> vi;
-typedef vector<ii>  vii;
 const int N = 1e5 + 50;
-const ll mod = 1e9 + 7;
-const double E = 1e-9;
 bool ok;
 int vis[N];
 vi G[N];
@@ -37,7 +17,7 @@ Time:
 
 void dfs(int u){
     vis[u] = 1;
-    for(int i = 0; i < sz(G[u]); i++){
+    for(int i = 0; i < (int) G[u].size(); i++){
         int v = G[u][i];
         if(vis[v] == -1){
             parent[v] = u;
@@ -47,23 +27,20 @@ void dfs(int u){
     }
 }
 int main(){
-    //FL;
-    FIN;
     int n, m;
     cin >> n >> m;
     while(m--){
         int u, v;
         cin >> u >> v;
         u--;v--;
-        G[u].pb(v);
-        G[v].pb(u);
+        G[u].push_back(v);
+        G[v].push_back(u);
     }
-    cls(vis, -1);
+    memset(vis, -1, sizeof vis);
     ok = 0;
     for(int i = 0; i < n; i++)
         if(vis[i] == -1)
             dfs(i);
     cout << (ok ? "cycle" : "not cycle") << '\n';
-    EjecuteTime;
     return 0;
 }       
