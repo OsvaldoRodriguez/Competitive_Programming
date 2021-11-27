24095b6bc810d4443c4d9b561ae1999ebd6c8a13
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2021-11-26 21:08:56 -0400

    Update Sparce_table.cpp

--- Notebook/data_structures/Sparce_table.cpp
+++ Notebook/data_structures/Sparce_table.cpp
@@ -1,28 +1,7 @@
 #include <bits/stdc++.h>
-#define FIN         ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define ii          pair<int,int>
-#define F           first
-#define S           second
-#define pb          push_back
-#define all(x)      (x).begin(),(x).end()
-#define rall(x)     (x).rbegin(),(x).rend()
-#define fore(i,a,b) for(int i = a;i < b; i+= 1)
-#define forr(i,a)   for(int i = a; i >= 0; i--)
-#define fori(i,m)   for(auto i = m.begin(); i != m.end(); i++) 
-#define w(t)        while(t--)
-#define sz(s)       int(s.size())
-#define cls(a,car)  memset(a,car,sizeof (a))
-#define db(x)       cerr << #x << " is " << x << '\n'
-#define FL          freopen("input.txt", "r", stdin),freopen("output.txt", "w", stdout);
-#define EjecuteTime cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n"
-#define angle(x)    double(x * acos(-1) / 180.0)
 using namespace std;
 typedef long long   ll;
-typedef vector<int> vi;
-typedef vector<ii>  vii;
 const int N = 1e5 + 50;
-const ll mod = 1e9 + 7;
-const double E = 1e-9;
 int A[N], ST[20][N];
 int n;
 
@@ -33,7 +12,6 @@ Space:
 Time:
     build   ->  O(log (n) * n)
     query   -> O(1)
-
 */
 int f(int a, int b){return __gcd(a, b);}
 void build(){
@@ -54,8 +32,6 @@ int query(int I, int J){
     return f(x, y);
 }
 int main(){
-    //FL;
-    FIN;
     int q;
     cin >> n;
     for(int i = 0; i < n; i++)
@@ -66,8 +42,7 @@ int main(){
     cin >> q;
     while(q--){
         cin >> l >> r;
-        cout << query(l - 1, r - 1) << '\n';
+        cout << query(l - 1, r - 1) << '\n'; // index 0
     }   
-    EjecuteTime;
     return 0;
 }       
