8a665b0dfd7e01113cd76f8f443e887f51a9e3f0
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2021-11-26 21:32:10 -0400

    Update Kruskal.cpp

--- Notebook/graphs/Kruskal.cpp
+++ Notebook/graphs/Kruskal.cpp
@@ -1,28 +1,7 @@
 #include <bits/stdc++.h>
-#define FIN         ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define ii          pair<int,int>
-#define F           first
-#define S           second
-#define pb          push_back
-#define all(x)      (x).begin(),(x).end()
-#define rall(x)     (x).rbegin(),(x).rend()
-#define fore(i,a,b) for(int i = a;i < b; i+= 1)
-#define forr(i,a)   for(int i = a; i >= 0; i--)
-#define fori(i,m)   for(auto i = m.begin(); i != m.end(); i++) 
-#define w(t)        while(t--)
-#define sz(s)       int(s.size())
-#define cls(a,car)  memset(a,car,sizeof (a))
-#define db(x)       cerr << #x << " is " << x << '\n'
-#define FL          freopen("input.txt", "r", stdin),freopen("output.txt", "w", stdout);
-#define EjecuteTime cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n"
-#define angle(x)    double(x * acos(-1) / 180.0)
 using namespace std;
 typedef long long   ll;
-typedef vector<int> vi;
-typedef vector<ii>  vii;
 const int N = 1e5 + 50;
-const ll mod = 1e9 + 7;
-const double E = 1e-9;
 vector<tuple<int, int, int>> G; // (w, u, v)
 int P[N], rnk[N];
 int n, m;
@@ -43,7 +22,7 @@ void init(){
 int kruskal(){
     init(); // disjoint set union
     ll ans = 0;
-    sort(all(G));
+    sort(G.begin(), G.end());
     for(int i = 0; i < m; i++){
         int w, u, v;
         tie(w, u, v) = G[i];
@@ -61,18 +40,13 @@ int kruskal(){
     return ans;
 }
 
-
-
 int main(){
-    //FL;
-    FIN;
     cin >> n >> m;
     for(int i = 0; i < m; i++){
         int u, v, w;
         cin >> u >> v >> w;
-        G.pb({w, u, v});
+        G.push_back({w, u, v});
     }
     cout << kruskal() << '\n';
-    EjecuteTime;
     return 0;
 }       
