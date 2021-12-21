babc6a3016ded93a9f7d42bd363e2ad64596b2ad
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2021-12-21 12:11:40 -0400

    Create A. Toy Army.cpp

---
+++ Contests/Codeforces/Codeforces Beta Round #72 (Div. 2 Only)/A. Toy Army.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+typedef long long ll;
+const int N = 100005;
+int main(){
+  int n;
+  while(cin >> n)
+    cout << n / 2 * 3 << "\n";
+  return 0;
+}
