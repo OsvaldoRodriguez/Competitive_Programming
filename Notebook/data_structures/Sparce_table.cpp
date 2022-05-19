29cf1ef1e03c266e05aad380d59d68b365ce0488
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2022-05-18 21:25:04 -0400

    Update Sparce_table.cpp

--- Notebook/data_structures/Sparce_table.cpp
+++ Notebook/data_structures/Sparce_table.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long   ll;
-const int N = 1e5 + 50;
-int A[N], ST[20][N];
+const int N = 1e5 + 10;
+const int LOG = 20;
+int A[N], ST[LOG + 1][N];
+int logs2[N];
 int n;
+
 
 /*
 Complextity
@@ -11,39 +13,64 @@ Space:
     build   ->  O(log(n) * n)
 Time:
     build   ->  O(log (n) * n)
-    query   -> O(1)
+    _query  -> O(1)
+    query   -> O(log n) 
 */
-int f(int a, int b){return __gcd(a, b);}
+
+int f(int a, int b){
+    return min(a, b);
+}
+
+// Complexity O(log n)
+int query(int L, int R){
+    int len = R - L + 1;
+    int ans = 1e9;
+    for(int i = LOG; i >= 0; i--){
+        if((1 << i) & len){
+            ans = f(ans, ST[i][L]);
+            L += (1 << i);
+        }
+    }
+    return ans;
+}
+// Complexity O(1)
+int _query(int L, int R){
+    int len = R - L + 1;
+    // int log = 32 - __builtin_clz(len) - 1;
+    int log = logs2[len];
+    return f(ST[log][L], ST[log][R - (1 << log) + 1]);
+}
+
 void build(){
+    logs2[1] = 0;
+    for(int i = 2; i < N; i++)
+        logs2[i] = logs2[i / 2] + 1;
+
     for(int i = 0; i < n; i++)
         ST[0][i] = A[i];
-    for(int i = 1; (1 << i) <= n; i++){// i <= log_2(n)
-        // j + (1 << i) - 1 el rango que cubre tiene que ser menor a n 
-        for(int j = 0; j < n - (1 << i) + 1; j++){
-            int a = j;
-            int b = j + (1 << (i - 1));
-            ST[i][j] = f(ST[i - 1][a], ST[i - 1][b]);
+    for(int i = 1; (1 << i) <= n; i++){
+        for(int j = 0; j + (1 << i) - 1 < n; j++){
+            ST[i][j] = f(ST[i - 1][j], ST[i - 1][j + (1 << (i - 1))]);
         }
     }
 }
-int query(int I, int J){
-    int a = 31 - __builtin_clz(J - I + 1);
-    int x = ST[a][I];
-    int y = ST[a][J - (1 << a) + 1];
-    return f(x, y);
-}
-int main(){
-    int q;
+int main (){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     cin >> n;
     for(int i = 0; i < n; i++)
         cin >> A[i];
-    int l, r;
     build();
     
+    int q;
     cin >> q;
     while(q--){
-        cin >> l >> r;
-        cout << query(l - 1, r - 1) << '\n'; // index 0
-    }   
+        int L, R;
+        cin >> L >> R;
+        // --L; --R;
+        cout << _query(L, R) << '\n';
+    }
     return 0;
-}       
+}
+
