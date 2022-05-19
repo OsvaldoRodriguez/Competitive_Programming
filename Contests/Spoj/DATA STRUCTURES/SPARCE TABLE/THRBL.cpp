41b2d3a41809beae9721375fa7120496a015e30e
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2022-05-19 10:57:51 -0400

    Create THRBL.cpp

---
+++ Contests/Spoj/DATA STRUCTURES/SPARCE TABLE/THRBL.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e5 + 10;
+const int LOG = 20;
+int A[N], ST[LOG + 1][N];
+int logs2[N];
+int n;
+ 
+ 
+/*
+Complextity
+Space: 
+    build   ->  O(log(n) * n)
+Time:
+    build   ->  O(log (n) * n)
+    _query  -> O(1)
+    query   -> O(log n) 
+*/
+ 
+int f(int a, int b){
+    return max(a, b);
+}
+ 
+// Complexity O(log n)
+int query(int L, int R){
+     int len = R - L + 1;
+    int ans = 1e9;
+    for(int i = LOG; i >= 0; i--){
+        if((1 << i) & len){
+            ans = f(ans, ST[i][L]);
+            L += (1 << i);
+        }
+    }
+    return ans;
+}
+// Complexity O(1)
+int _query(int L, int R){
+    int len = R - L + 1;
+    // int log = 32 - __builtin_clz(len) - 1;
+    int log = logs2[len];
+    return f(ST[log][L], ST[log][R - (1 << log) + 1]);
+}
+ 
+void build(){
+ 
+    logs2[1] = 0;
+    for(int i = 2; i < N; i++)
+        logs2[i] = logs2[i / 2] + 1;
+ 
+    for(int i = 0; i < n; i++)
+        ST[0][i] = A[i];
+    for(int i = 1; (1 << i) <= n; i++){
+        for(int j = 0; j + (1 << i) - 1 < n; j++){
+            ST[i][j] = f(ST[i - 1][j], ST[i - 1][j + (1 << (i - 1))]);
+        }
+    }
+}
+int main (){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int q;
+    cin >> n;
+    cin >> q;
+    for(int i = 0; i < n; i++)
+        cin >> A[i];
+    build();
+    
+    int ans = 0;
+    while(q--){
+        int L, R;
+        cin >> L >> R;
+        --L; --R;
+        int a = min(L, R);
+        int b = max(L, R);
+        if(A[a] >= _query(a, b - 1))
+            ans++;
+    }
+    cout << ans << '\n';
+    return 0;
+}
