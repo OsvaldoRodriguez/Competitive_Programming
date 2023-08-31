ebe4b41f1a1700f884de58243150d47aa8d560fe
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2023-08-31 17:16:14 -0400

    Create 11310 - Delivery Debacle.cpp

---
+++ Contests/Uva/Competitive Programming 3/Mathematics/Combinatorics/Others, Easier/11310 - Delivery Debacle.cpp
@@ -0,0 +1,58 @@
+#include "bits/stdc++.h"
+// #include <ext/pb_ds/assoc_container.hpp> // Common file
+// #include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+#define fastio      ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define ll          long long
+#define all(_)      (_).begin(), (_).end()
+#define sz(_)       int(_.size())
+#define pii         pair<int, int>
+#define pll         pair<ll, ll>
+using namespace std;
+// using namespace __gnu_pbds; // ordered_set
+const int N = 1e6 + 5;
+ll mod = 998244353;
+const double eps = 1e-9;
+// less<type>  prioridad menor
+// greater<type>  prioridad mayor
+// add equal<type>  less_equal or greater_equal // multiset
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+int di[] = {1, 0, 0, -1}, dj[] = {0, -1, 1, 0};
+// string dir = "DLRU";
+//**********************************************************************
+// Later or Sooner discipline overcomes talent, come on, I can do it.
+ll dp[45];
+ll f(int i){
+    if(i < 0)
+        return 0;
+    if(i == 0)
+        return 1;
+
+    ll &ans = dp[i];
+    if(ans == -1){
+        ans = 0;
+        ans += f(i - 1) + 4 * f(i - 2) + 2 * f(i - 3);
+    }
+    return ans;
+}
+void solve(){
+    ll n;
+    cin >> n;
+    memset(dp, -1, sizeof dp);
+    cout << f(n) << "\n";
+}
+
+
+int main(){
+    fastio;
+    int t = 1;
+    cin >> t;
+    for(int i = 1; i <= t; i++)
+        solve();
+    
+    return 0;
+}   
+// solo recorre en los bits encendidos
+// O(bits_encendidos en mask)
+// for(int x = mask; x; x &= (x - 1)){
+//     int v = __builtin_ctz(x); // da la posicion donde esta 
+// }
