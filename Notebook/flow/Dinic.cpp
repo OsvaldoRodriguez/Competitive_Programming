2a86d05c498f2220b99db7ae55e81531d4314823
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2023-10-10 17:29:36 -0400

    Update Dinic.cpp

--- Notebook/flow/Dinic.cpp
+++ Notebook/flow/Dinic.cpp
@@ -11,18 +11,30 @@ V: vertices
 E: aristas
 */
 
+// Min cut: nodes with dist>=0 vs nodes with dist<0
+// Matching MVC: left nodes with dist<0 + right nodes with dist>0
 struct Dinic{
     struct Edge{
+        // rev is the index in the graph G ie.
+        // if a edge is (to, rev, cap, flow) then, if the edge is of the graph then rev is the 
+        // index of edge in the residual graph
+        // if si the edge is of the residual graph then rev is the index of edge in the graph
         int to, rev;
         ll cap, flow;
         Edge(int to, int rev, ll cap, ll flow): to(to), rev(rev), cap(cap), flow(flow){}
     };
     vector<vector<Edge>> G;
     vector<int> dis, block;
-    int n, s, t;
+    int n, s, t;    
     Dinic(){}
     Dinic(int n, int s, int t): n(n), dis(n), block(n), s(s), t(t), G(n){}
     void addEdge(int u, int v, ll cap){
+        // rev -> is the index in the graph where is the edge of residual graph
+        // or viceversa
+        // that's why the first edge add is G[v].size(), because v's length is the index where it'll 
+        // be 
+        // that's why the second edge add is G[u].size() - 1, because u's length - 1 is
+        // the index where it've been to add.
         G[u].push_back((Edge){v, (int) G[v].size(), cap, 0});
         G[v].push_back((Edge){u, (int) G[u].size() - 1, 0, 0});
     }
@@ -65,8 +77,23 @@ struct Dinic{
             fill(block.begin(), block.end(), 0);
             while(ll _cur = dfs(s, LLONG_MAX))
                 ans += _cur;
+            
         }
         return ans;
+    }
+
+    void mostrar(){
+        cout << "\ngraph\n";
+        for(int i = 0; i < G.size(); i++){
+            if(G[i].size()){
+                cout << i << " -> ";
+                for(auto j : G[i]){
+                    cout << "(" << j.to << " " << j.rev << " " << j.cap << " " << j.flow << ")";
+                }
+                cout << "\n";
+            }
+        }
+        cout << endl;
     }
 };
 void solve(){
